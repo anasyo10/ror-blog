b4b6651d2796d8cc8487688b3afa73952d93765a
anasyo10 <anasyo10@gmail.com>
2017-11-27 19:51:53 +0500

    categories created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only:[:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -28,11 +29,19 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to FP Blog #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
         end    
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy 
+      flash[:danger] = "The users all data and articles have been deleted."
+      redirect_to users_path
     end
 
     private
@@ -44,9 +53,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
      end
      def require_same_user
-         if current_user != @user
+         if current_user != @user and !current_user.admin?
              flash[:danger] = "Please login in to your own account and manage your own profile?"
              redirect_to root_path
          end
      end
+
+     def require_admin
+        if !current_user.admin?
+            flash[:danger] = "only admin can do this."
+            redirect_to root_path            
+     end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
-
-
+  
+  
   resources :articles
-
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories, except: [:destroy]
 end
