cf4fb8b4c25fba49347b8b4e3df1609db4fc5d14
anasyo10 <anasyo10@gmail.com>
2017-11-28 15:39:21 +0500

    Categories added with many to many relation

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllertest < ActionDispatch::IntegrationTest
     def setup
         @category = Category.create(name: "sports")
+        @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)        
     end
 
     test "should get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllertest < ActionDispatch::IntegrationTest
 
 
     test "should get categories new" do
+        sign_in_as(@user, "password")
         get new_category_path
         assert_response :success
     end
@@ -19,5 +21,12 @@ class CategoriesControllertest < ActionDispatch::IntegrationTest
     test "should get categories show" do
         get category_path(@category)
         assert_response :success
+    end
+
+    test "should redirect create when admin not logged in" do
+        assert_no_difference 'Category.count' do
+            post categories_path, params: { category: { name: "Food" } }
+        end
+        assert_redirected_to categories_path
     end
 end
